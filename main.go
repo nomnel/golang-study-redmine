d6cadfbbb3a43acb6f9db5b8a491812006a54afb
nomnel <laxyz.nomnel@gmail.com>
2018-07-08 05:54:27 +0900

    add fatal method and use it

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"os"
 )
 
 type config struct {
@@ -19,13 +20,22 @@ func getConfig() config {
 	filename := "settings.json"
 	b, err := ioutil.ReadFile(filename)
 	if err != nil {
-		panic("Panic!")
+		fatal("Failed to read config file: %s\n", err)
 	}
 
 	var c config
 	err = json.Unmarshal(b, &c)
 	if err != nil {
-		panic("Panic!")
+		fatal("Failed to unmarshal file: %s\n", err)
 	}
 	return c
+}
+
+func fatal(format string, err error) {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, format, err)
+	} else {
+		fmt.Fprint(os.Stderr, format)
+	}
+	os.Exit(1)
 }
