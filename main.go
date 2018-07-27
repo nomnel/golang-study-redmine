d91cecb294bff2c9944dc9eefe07bc8b29a4ca48
nomnel <laxyz.nomnel@gmail.com>
2018-07-27 10:51:03 +0900

    switch operations by flags

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -12,18 +13,29 @@ type config struct {
 	Apikey   string `json:"apikey"`
 }
 
+var conf config
+
 func main() {
-	c := getConfig()
-	client := newClient(c.Endpoint, c.Apikey)
-	filter := &issueFilter{
-		AssignedToID: "me",
-	}
-	issues, err := client.issuesByFilter(filter)
-	if err != nil {
-		fatal("Failed to list issues: %s\n", err)
-	}
-	for _, i := range issues {
-		fmt.Printf("%4d: %s\n", i.ID, i.Subject)
+	conf = getConfig()
+
+	flag.Parse()
+	switch flag.Arg(0) {
+	case "i", "issue":
+		switch flag.Arg(1) {
+		case "l", "list":
+			listIssues(nil)
+			break
+		case "m", "mine":
+			filter := issueFilter{
+				AssignedToID: "me",
+			}
+			listIssues(&filter)
+			break
+		default:
+			usage()
+		}
+	default:
+		usage()
 	}
 }
 
@@ -48,5 +60,27 @@ func fatal(format string, err error) {
 	} else {
 		fmt.Fprint(os.Stderr, format)
 	}
+	os.Exit(1)
+}
+
+func listIssues(filter *issueFilter) {
+	c := newClient(conf.Endpoint, conf.Apikey)
+	issues, err := c.issuesByFilter(filter)
+	if err != nil {
+		fatal("Failed to list issues: %s\n", err)
+	}
+	for _, i := range issues {
+		fmt.Printf("%4d: %s\n", i.ID, i.Subject)
+	}
+}
+
+func usage() {
+	fmt.Println(`./golang-study-redmine <command>
+
+Issue Commands:
+  list     l listing issues.
+             $ ./golang-study-redmine i l
+  mine     m listing your issues.
+             $ ./golang-study-redmine i m`)
 	os.Exit(1)
 }
